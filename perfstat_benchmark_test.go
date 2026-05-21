61fc07096e3b25cf4ed1ebf6fc7857a1ee2eb7bb
allocz <alloczr@gmail.com>
2026-05-21 21:48:44 +0000

    add benchmark to confirm that there's no allocations in hot loop execution

---
+++ perfstat_benchmark_test.go
@@ -0,0 +1,28 @@
+package perfstat_test
+
+import (
+	"testing"
+
+	"github.com/allocz/perfstat"
+)
+
+// As a Global to avoid compiler optimizing this out
+var s perfstat.Stats
+
+func BenchmarkPerfstatStartTimer(b *testing.B) {
+	s := perfstat.Stats{}
+	for range b.N {
+		endR := s.StartTimer("root")
+
+		end := s.StartTimer("label1")
+		end.Finish()
+
+		end = s.StartTimer("label2")
+		end.Finish()
+
+		end = s.StartTimer("label3")
+		end.Finish()
+
+		endR.Finish()
+	}
+}
